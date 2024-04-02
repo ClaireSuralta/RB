c94ef20e718b27df393adf263fcbab623fee272a
RochilleNabong <nabongrochille@gmail.com>
2024-04-02 15:27:17 +0800

    Success

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,6 @@
 function search(search_value){
     $.ajax({
-        url: '/Claire/model/searchEngine.php',
+        url: '/RB/model/searchEngine.php',
         type: 'POST',
         data: { 'search' : search_value },
         success: function(response){
